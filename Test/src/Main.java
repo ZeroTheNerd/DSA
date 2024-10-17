cc7d8e3905de7a77f77044735d8ee9a38fa0dc92
ZachMartim <zachmartim101@gmail.com>
2024-10-16 21:47:46 -0600

    Test Pointers in Java with Hashmaps

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,6 +9,19 @@ public class Main {
         System.out.println(cookieTwo.getColor());
         cookieOne.setColor("yellow");
         System.out.println(cookieOne.getColor());
+
+
+        //Map's store pointers to their data. Confirm that here
+        HashMap<String, Integer> map1 = new HashMap<>();
+        HashMap<String, Integer> map2 = new HashMap<>();
+
+        map1.put("value", 11);
+        map2 = map1;
+
+        map1.put("valueTwo", 22);
+        System.out.println(map1);
+        map1.put("value", 15);
+        System.out.println(map2);
 
     }
 }
