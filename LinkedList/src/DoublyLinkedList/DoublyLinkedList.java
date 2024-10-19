4beae95c9442c040915ad8b13058bebb5ec6dea8
ZachMartim <zachmartim101@gmail.com>
2024-10-19 16:15:56 -0600

    DLL get - Completed

--- LinkedList/src/DoublyLinkedList/DoublyLinkedList.java
+++ LinkedList/src/DoublyLinkedList/DoublyLinkedList.java
@@ -68,7 +68,7 @@ public class DoublyLinkedList {
         }
         length++;
     }
-    
+
     public Node removeFirst() {
         if(length == 0) {
             return null;
@@ -85,6 +85,26 @@ public class DoublyLinkedList {
         }
         length--;
         return temp;
+    }
+
+    public Node get(int index) {
+        if(index < 0 || index >= length) {
+            return null;
+        }
+        Node temp = head;
+        if(index < length / 2) {
+            //temp = head;
+            for(int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+        } else {
+            temp = tail;
+            for(int i = length - 1; i > index; i--) {
+                temp = temp.prev;
+            }
+        }
+        return temp;
+
     }
 
 
