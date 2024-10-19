a9f4b0710509f882b97cab0c94fe9663b4bb327d
ZachMartim <zachmartim101@gmail.com>
2024-10-19 17:01:32 -0600

    DLL swapPairs - Completed

--- LinkedList/src/DoublyLinkedList/DoublyLinkedList.java
+++ LinkedList/src/DoublyLinkedList/DoublyLinkedList.java
@@ -200,6 +200,34 @@ public class DoublyLinkedList {
         return true;
     }
 
+    public void swapPairs() {
+        Node dummyNode = new Node(0);
+        dummyNode.next = head;
+        Node previousNode = dummyNode;
+
+        while (head != null && head.next != null) {
+            Node firstNode = head;
+            Node secondNode = head.next;
+
+            previousNode.next = secondNode;
+            firstNode.next = secondNode.next;
+            secondNode.next = firstNode;
+
+            secondNode.prev = previousNode;
+            firstNode.prev = secondNode;
+
+            if (firstNode.next != null) {
+                firstNode.next.prev = firstNode;
+            }
+
+            head = firstNode.next;
+            previousNode = firstNode;
+        }
+
+        head = dummyNode.next;
+        if (head != null) head.prev = null;
+    }
+
 
     public void printList() {
         Node temp = head;
