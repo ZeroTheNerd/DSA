5bcc464f89deaa8a6008b3240d0e662f4ebf27e4
ZachMartim <zachmartim101@gmail.com>
2024-10-19 15:39:19 -0600

    DoublyLinkedList - Set up

---
+++ LinkedList/src/DoublyLinkedList/Main.java
@@ -0,0 +1,15 @@
+package DoublyLinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DoublyLinkedList dll = new DoublyLinkedList(7);
+
+        dll.getHead();
+        dll.getTail();
+        dll.getLength();
+
+        dll.printList();
+    }
+
+}
