933364987994aecdc3a44266220f465ea6c939c9
ZachMartim <zachmartim101@gmail.com>
2024-10-19 16:49:37 -0600

    DLL isPalindrome - Completed

--- LinkedList/src/DoublyLinkedList/DoublyLinkedList.java
+++ LinkedList/src/DoublyLinkedList/DoublyLinkedList.java
@@ -183,6 +183,23 @@ public class DoublyLinkedList {
         tail = temp;
     }
 
+    public boolean isPalindrome() {
+        if(length <= 1) {
+            return true;
+        }
+        Node forward = head;
+        Node backward = tail;
+
+        for(int i = 0; i < length / 2; i++) {
+            if(forward.value != backward.value) {
+                return false;
+            }
+            forward = forward.next;
+            backward = backward.prev;
+        }
+        return true;
+    }
+
 
     public void printList() {
         Node temp = head;
