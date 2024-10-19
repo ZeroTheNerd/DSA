67537a87994a78a6ad8ce8802471a56824a4d4cc
ZachMartim <zachmartim101@gmail.com>
2024-10-19 14:21:11 -0600

    hasLoop - Completed

--- LinkedList/src/LinkedList/MyLinkedList.java
+++ LinkedList/src/LinkedList/MyLinkedList.java
@@ -163,6 +163,19 @@ public class MyLinkedList {
         }
         return slowTemp;
     }
+
+    public boolean hasLoop() {
+        Node fast = head;
+        Node slow = head;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
     public void getHead() {
         System.out.println("Head: " + head.value);
     }
