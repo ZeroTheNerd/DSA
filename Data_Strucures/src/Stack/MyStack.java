44b2373b1739270724f158de4f8f1319156b2654
ZachMartim <zachmartim101@gmail.com>
2024-10-19 21:41:07 -0600

    Stack push - Completed

--- Data_Strucures/src/Stack/MyStack.java
+++ Data_Strucures/src/Stack/MyStack.java
@@ -37,4 +37,15 @@ public class MyStack {
             }
         }
     }
+
+    public void push(int value) {
+        Node newNode = new Node(value);
+        if(height == 0) {
+            top = newNode;
+        } else {
+            newNode.next = top;
+            top = newNode;
+        }
+        height++;
+    }
 }
