9bd40c8cc6651cdd8b74d360dacec27257cca478
ZachMartim <zachmartim101@gmail.com>
2024-10-27 10:42:08 -0600

    Start Hash Table DS - print & hash methods - Completed

---
+++ Data_Strucures/src/HashTable/HashTable.java
@@ -0,0 +1,50 @@
+package HashTable;
+
+
+// Hash Representation - Using seperate chaining with Linked List
+// "{
+//    "key"= "bolts",
+//    "value"= 400,
+//    "next"= null
+//  }"
+
+public class HashTable {
+    public int size = 7;
+    private Node[] dataMap;
+
+    public class Node {
+        int value;
+        private String key;
+        private Node next;
+
+        public Node(String key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public HashTable() {
+        dataMap = new Node[size];
+    }
+
+    public void printTable() {
+        for(int i = 0; i < dataMap.length; i++) {
+            System.out.println(i + ":");
+            Node temp = dataMap[i];
+            while (temp != null) {
+                System.out.println(" {" + temp.key + "= " + temp.value + "}");
+                temp = temp.next;
+            }
+        }
+    }
+
+    private int hash(String key) {
+        int hash = 0;
+        char[] keyChars = key.toCharArray();
+        for(int i = 0; i < keyChars.length; i++) {
+            int asciiVal = keyChars[i];
+            hash = (hash + asciiVal * 23) % dataMap.length;
+        }
+        return hash;
+    }
+}
