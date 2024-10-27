65b5e47d53dc5dd61e2e0dd2ce419a6fd23770f8
ZachMartim <zachmartim101@gmail.com>
2024-10-26 18:34:44 -0600

    BST contains - Completed

--- Data_Strucures/src/BST/BST.java
+++ Data_Strucures/src/BST/BST.java
@@ -39,5 +39,22 @@ public class BST {
         }
     }
 
+    public boolean contains(int value) {
+        if(root == null) {
+            return false;
+        }
+        Node temp = root;
+        while(temp != null) {
+            if(value < temp.value) {
+                temp = temp.left;
+            } else if(value > temp.value) {
+                temp = temp.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 }
