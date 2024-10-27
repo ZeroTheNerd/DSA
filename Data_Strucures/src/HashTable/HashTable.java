d797ed8e9cd5da53f522e0f3765cfb3d86fcb665
ZachMartim <zachmartim101@gmail.com>
2024-10-27 11:37:39 -0600

    Some HashMap leetcode problems

--- Data_Strucures/src/HashTable/HashTable.java
+++ Data_Strucures/src/HashTable/HashTable.java
@@ -9,6 +9,9 @@ package HashTable;
 //  }"
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class HashTable {
     public int size = 7;
@@ -88,5 +91,45 @@ public class HashTable {
             }
         }
         return keys;
+    }
+
+    public boolean itemInCommon(int[] arr1, int[] arr2) {
+        HashMap<Integer, Boolean> myHashMap = new HashMap<>();
+        for(int i : arr1) {
+            myHashMap.put(i, true);
+        }
+
+        for(int j : arr2) {
+            if (myHashMap.get(j) != null) return true;
+        }
+        return false;
+    }
+
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> duplicates = new ArrayList<>();
+        Map<Integer, Integer> numCount = new HashMap<>();
+        for(int num : nums) {
+            numCount.put(num, numCount.getOrDefault(num, 0) + 1);
+        }
+        for(Map.Entry<Integer, Integer> entry : numCount.entrySet()) {
+            if (entry.getValue() > 1) {
+                duplicates.add(entry.getKey());
+            }
+        }
+        return duplicates;
+
+    }
+
+    public Character firstNonRepeatingChar(String string) {
+        Map<Character, Integer> letCounts = new HashMap<>();
+        for(char c : string.toCharArray()) {
+            letCounts.put(c, letCounts.getOrDefault(c, 0) + 1);
+        }
+
+        for(char c : string.toCharArray()) {
+            if(letCounts.get(c) == 1) return c;
+        }
+        return null;
+
     }
 }
